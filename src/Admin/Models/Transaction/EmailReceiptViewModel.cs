8ca1dfc288d2b74c4bb48c5162248127d39d8c16
BMBC Dev <94853739+bmbc-dev-detec@users.noreply.github.com>
2022-01-17 11:30:04 +0000

    Updates to placeholder values to include an action (#73)

--- src/Admin/Models/Transaction/EmailReceiptViewModel.cs
+++ src/Admin/Models/Transaction/EmailReceiptViewModel.cs
@@ -4,7 +4,7 @@ namespace Admin.Models.Transaction
 {
     public class EmailReceiptViewModel
     {
-        [Display(Name = "Please enter the recipients e-mail address")]
+        [Display(Name = "Please enter the recipients email address")]
         public string EmailAddress { get; set; }
         public string PspReference { get; set; }
     }
