549e5254127a2b19efbc848089c798eab621fe62
BMBC Dev <bmbc.dev@detec.software>
2022-07-27 16:23:18 +0100

    Remove PendingTransaction BatchReference field

---
+++ src/DataAccess/Migrations/202207271522024_Remove_PendingTransaction_BatchReference.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Remove_PendingTransaction_BatchReference : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.PendingTransactions", "BatchReference");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.PendingTransactions", "BatchReference", c => c.String(maxLength: 30));
+        }
+    }
+}
