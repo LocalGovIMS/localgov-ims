5e15c43c5900122632fbc1d763277462725d29f3
BMBC Dev <94853739+bmbc-dev-detec@users.noreply.github.com>
2022-08-03 12:16:47 +0100

    When no fund supplied during transaction import send transaction to suspense (#306)

---
+++ src/BusinessLogic.UnitTests/Services/Suspense/TestBase.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLogic.Interfaces.Persistence;
+using BusinessLogic.Interfaces.Security;
+using BusinessLogic.Interfaces.Strategies;
+using log4net;
+using Moq;
+using System.Diagnostics.CodeAnalysis;
+using Service = BusinessLogic.Services.SuspenseService;
+
+namespace BusinessLogic.UnitTests.Services.Suspense
+{
+    [ExcludeFromCodeCoverage]
+    public abstract class TestBase
+    {
+        protected readonly Mock<ILog> MockLogger = new Mock<ILog>();
+        protected readonly Mock<IUnitOfWork> MockUnitOfWork = new Mock<IUnitOfWork>();
+        protected readonly Mock<ISecurityContext> MockSecurityContext = new Mock<ISecurityContext>();
+        protected readonly Mock<IJournalAllocationStrategy> MockJournalAllocationStrategy = new Mock<IJournalAllocationStrategy>();
+
+        protected Service GetService()
+        {
+            var service = new Service(
+                MockLogger.Object,
+                MockUnitOfWork.Object,
+                MockSecurityContext.Object,
+                MockJournalAllocationStrategy.Object);
+
+            return service;
+        }
+    }
+}
