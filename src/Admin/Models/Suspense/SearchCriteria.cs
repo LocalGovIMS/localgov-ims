a358380318c07fcb225453dbc51b79cc95e583fd
BMBC Dev <bmbc.dev@detec.software>
2022-08-16 15:28:22 +0100

    Suspense pages

--- src/Admin/Models/Suspense/SearchCriteria.cs
+++ src/Admin/Models/Suspense/SearchCriteria.cs
@@ -1,15 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Admin.Models.Suspense
 {
     public class SearchCriteria
     {
+        [Display(Name = "Start date")]
         public DateTime? TransactionDateFrom { get; set; }
+
+        [Display(Name = "End date")]
         public DateTime? TransactionDateTo { get; set; }
+
+        [Display(Name = "Account number")]
         public string AccountNumber { get; set; }
+
         public string Narrative { get; set; }
+
         public decimal? Amount { get; set; }
+
+        [Display(Name = "Show all")]
         public bool ShowAll { get; set; }
+
         public int Page { get; set; }
     }
 }
