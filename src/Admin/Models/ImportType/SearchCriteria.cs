61dfabbbac0b828943f45ba446beba12d2ad05ec
BMBC Dev <bmbc.dev@detec.software>
2022-08-16 14:18:14 +0100

    Import Type pages

--- src/Admin/Models/ImportType/SearchCriteria.cs
+++ src/Admin/Models/ImportType/SearchCriteria.cs
@@ -1,12 +1,18 @@
 ﻿using BusinessLogic.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Admin.Models.ImportType
 {
     public class SearchCriteria
     {
+        [Display(Name = "Data type")]
         public ImportDataTypeEnum DataType { get; set; }
+
         public string Name { get; set; }
+
+        [Display(Name = "External reference")]
         public string ExternalReference { get; set; }
+
         public int Page { get; set; }
     }
 }
---
+++ src/BusinessLogic/Extensions/BooleanExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace BusinessLogic.Extensions
+{
+    public static class BooleanExtensions
+    {
+        public static string ToYesNoString(this bool value)
+        {
+            return value ? "Yes" : "No";
+        }
+
+        public static string ToTrueFalseString(this bool value)
+        {
+            return value ? "True" : "False";
+        }
+    }
+}
