a585e33d8fd38146a16f8b6e02993263c972667b
BMBC Dev <bmbc.dev@detec.software>
2022-09-06 10:56:23 +0100

    Add additional details to the API FundModel

--- src/Api/Controllers/Funds/FundModel.cs
+++ src/Api/Controllers/Funds/FundModel.cs
@@ -10,12 +10,34 @@ namespace Api.Controllers.Funds
 
         public string FundName { get; set; }
 
+        public string DisplayName { get; set; }
+
+        public string VatCode { get; set; }
+
+        public decimal? MaximumAmount { get; set; }
+
+        public bool OverPayAccount { get; set; }
+
+        public bool AccountExist { get; set; }
+
+        public bool AquireAddress { get; set; }
+
+        public bool VatOverride { get; set; }
+
         public List<FundMetadataModel> Metadata { get; set; }
 
         public FundModel(BusinessLogic.Entities.Fund source)
         {
             FundCode = source.FundCode;
             FundName = source.FundName;
+            DisplayName = source.DisplayName;
+            VatCode = source.VatCode;
+            MaximumAmount = source.MaximumAmount;
+            OverPayAccount = source.OverPayAccount;
+            AccountExist = source.AccountExist;
+            AquireAddress = source.AquireAddress;
+            VatOverride = source.VatOverride;
+
             Metadata = source.Metadata?.Select(x => new FundMetadataModel(x)).ToList();
         }
     }
