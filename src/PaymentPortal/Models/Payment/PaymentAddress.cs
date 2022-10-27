c3f36ac7369dde7e0609db94de5e6979ce84ea2d
BMBC Dev <bmbc.dev@detec.software>
2022-10-27 10:19:00 +0100

    Add validation to make sure address fields contain letters and can include numbers

--- src/PaymentPortal/Models/Payment/PaymentAddress.cs
+++ src/PaymentPortal/Models/Payment/PaymentAddress.cs
@@ -6,32 +6,39 @@ namespace PaymentPortal.Models.Payment
     [Serializable]
     public class PaymentAddress
     {
-        [Required(ErrorMessage = "Payee Name is required")]
-        [Display(Name = "Payee Name")]
-        [MaxLength(50, ErrorMessage = "Payee Name cannot be longer than 50 characters")]
+        private const string IsNotJustNumericExpression = @"(?!^[\d\W]+$)^.+$";
+
+        [Required(ErrorMessage = "Payee name is required")]
+        [Display(Name = "Payee name")]
+        [MaxLength(50, ErrorMessage = "Payee name cannot be longer than 50 characters")]
+        [RegularExpression(IsNotJustNumericExpression, ErrorMessage="Payee name should contain letters and can include numbers")]
         public string PayeeName { get; set; }
 
         [Required(ErrorMessage = "Address line 1 is required")]
         [Display(Name = "Address line 1")]
         [MaxLength(50, ErrorMessage = "Address line 1 cannot be longer than 50 characters")]
+        [RegularExpression(IsNotJustNumericExpression, ErrorMessage = "Address line 1 should contain letters and can include numbers")]
         public string AddressLine1 { get; set; }
 
         [Display(Name = "Address line 2")]
         [MaxLength(50, ErrorMessage = "Address line 2 cannot be longer than 50 characters")]
+        [RegularExpression(IsNotJustNumericExpression, ErrorMessage = "Address line 2 should contain letters and can include numbers")]
         public string AddressLine2 { get; set; }
 
-        [Required(ErrorMessage = "Town or City is required")]
-        [Display(Name = "Town or City")]
-        [MaxLength(50, ErrorMessage = "Town or City cannot be longer than 50 characters")]
+        [Required(ErrorMessage = "Town or city is required")]
+        [Display(Name = "Town or city")]
+        [MaxLength(50, ErrorMessage = "Town or city cannot be longer than 50 characters")]
+        [RegularExpression(IsNotJustNumericExpression, ErrorMessage = "Town or city should contain letters and can include numbers")]
         public string AddressLine3 { get; set; }
 
         [Display(Name = "County")]
         [MaxLength(50, ErrorMessage = "County cannot be longer than 50 characters")]
+        [RegularExpression(IsNotJustNumericExpression, ErrorMessage = "County should contain letters and can include numbers")]
         public string AddressLine4 { get; set; }
 
-        [Required(ErrorMessage = "Postal Code is required")]
-        [Display(Name = "Postal Code")]
-        [MaxLength(10, ErrorMessage = "Postal Code cannot be longer than 10 characters")]
+        [Required(ErrorMessage = "Postcode is required")]
+        [Display(Name = "Postcode")]
+        [MaxLength(10, ErrorMessage = "Postcode cannot be longer than 10 characters")]
         public string PostCode { get; set; }
     }
 }
