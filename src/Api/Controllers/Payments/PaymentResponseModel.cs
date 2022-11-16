987355b420ea100d30cae84a6dfe24b88d47fbaa
BMBC Detec <bmbc.dev@detec.software>
2022-11-16 10:28:46 +0000

    Add ability to create payments via the API

---
+++ src/Api/Controllers/Payments/PaymentResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLogic.Classes;
+
+namespace Api.Controllers.PendingTransactions
+{
+    public class PaymentResponseModel
+    {
+        public string PaymentUrl { get; set; }
+        public string Reference { get; set; } // PendingTransaction.InternalReference
+
+        public PaymentResponseModel(PaymentResponse response)
+        {
+            PaymentUrl = response.ResponseUrl;
+            Reference = response.ResponseId;
+        }
+    }
+}
