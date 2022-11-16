ac17617d24509434039835864acb3d5a408dd165
BMBC Dev <94853739+bmbc-dev-detec@users.noreply.github.com>
2022-11-16 11:58:25 +0000

    Add ability to create payments via the API (#420)
    * Add ability to create payments via the API
    
    * Correct PaymentsController namespace
    
    * Update NSwag file to include PaymentsController

---
+++ src/Api/Controllers/Payments/PaymentModel.cs
@@ -0,0 +1,71 @@
+﻿using BusinessLogic.Classes;
+using System.Collections.Generic;
+
+namespace Api.Controllers.PendingTransactions
+{
+    public class PaymentModel
+    {
+        public Address Address { get; set; }
+
+        public List<BasketItem> Basket { get; set; }
+
+        public string SuccessUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public string FailUrl { get; set; }
+        public string MopCode { get; set; }
+        public string ExternalReference { get; set; }
+        public string CallRecordingSource { get; set; }
+        public string CallRecordingUserName { get; set; }
+
+        public List<PaymentDetails> GetPaymentDetails()
+        {
+            var paymentDetailsList = new List<PaymentDetails>();
+
+            foreach (var item in Basket)
+            {
+                paymentDetailsList.Add(new PaymentDetails()
+                {
+                    AccountReference = item.AccountReference,
+                    Fund = item.Fund,
+                    Amount = item.Amount,
+                    Narrative = item.Narrative,
+                    VatCode = item.VatCode,
+                    CancelUrl = CancelUrl,
+                    FailUrl = FailUrl,
+                    SuccessUrl = SuccessUrl,
+                    PayeeName = Address.PayeeName ?? string.Empty,
+                    AddressLine1 = Address.AddressLine1 ?? string.Empty,
+                    AddressLine2 = Address.AddressLine2 ?? string.Empty,
+                    AddressLine3 = Address.AddressLine3 ?? string.Empty,
+                    AddressLine4 = Address.AddressLine4 ?? string.Empty,
+                    Postcode = Address.Postcode ?? string.Empty,
+                    AppReference = ExternalReference,
+                    MopCode = MopCode,
+                    CallRecordingSource = CallRecordingSource,
+                    CallRecordingUserName = CallRecordingUserName
+                });
+            }
+
+            return paymentDetailsList;
+        }
+    }
+
+    public class Address
+    {
+        public string PayeeName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string AddressLine4 { get; set; }
+        public string Postcode { get; set; }
+    }
+
+    public class BasketItem
+    {
+        public string Fund { get; set; }
+        public decimal Amount { get; set; }
+        public string AccountReference { get; set; }
+        public string Narrative { get; set; }
+        public string VatCode { get; set; }
+    }
+}
---
+++ src/Api/Controllers/Payments/PaymentsController.cs
@@ -0,0 +1,45 @@
+﻿using Api.Controllers.PendingTransactions;
+using BusinessLogic.Interfaces.Services;
+using log4net;
+using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Web.Http;
+
+namespace Api.Controllers.Payments
+{
+    public class PaymentsController : ApiController
+    {
+        private readonly ILog _log;
+        private readonly IPaymentService _paymentService;
+
+        public PaymentsController(
+            ILog log,
+            IPaymentService paymentService)
+        {
+            _log = log ?? throw new ArgumentNullException("log");
+            _paymentService = paymentService ?? throw new ArgumentNullException("paymentService");
+        }
+
+        [HttpPost]
+        [SwaggerResponse(System.Net.HttpStatusCode.Created, "Created", typeof(PaymentResponseModel))]
+        [SwaggerResponse(System.Net.HttpStatusCode.BadRequest, "Bad request", typeof(string))]
+        [SwaggerResponseRemoveDefaults]
+        public IHttpActionResult Post([FromBody] PaymentModel model)
+        {
+            try
+            {
+                var result = _paymentService.CreateHppPayments(model.GetPaymentDetails());
+
+                var response = new PaymentResponseModel(result);
+                
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex);
+
+                return BadRequest();
+            }
+        }
+    }
+}
