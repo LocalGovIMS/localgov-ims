ce65cae97c17ae9c16724cfd0a2ec46eb01a03ed
BMBC Detec <bmbc.dev@detec.software>
2022-12-22 16:06:57 +0000

    Updates to Payment Portal

--- src/PaymentPortal/App_Start/BundleConfig.cs
+++ src/PaymentPortal/App_Start/BundleConfig.cs
@@ -10,13 +10,6 @@ namespace PaymentPortal
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.unobtrusive*",
-                "~/Scripts/jquery.validate*"));
-
             bundles.Add(new ScriptBundle("~/bundles/accessible-autocomplete")
                 .Include("~/Scripts/accessible-autocomplete.min.js"));
 
@@ -27,20 +20,6 @@ namespace PaymentPortal
                 "~/Content/bootstrap.css",
                 "~/Content/accessible-autocomplete.min.css",
                 "~/Content/app.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                "~/Content/themes/base/jquery.ui.core.css",
-                "~/Content/themes/base/jquery.ui.resizable.css",
-                "~/Content/themes/base/jquery.ui.selectable.css",
-                "~/Content/themes/base/jquery.ui.accordion.css",
-                "~/Content/themes/base/jquery.ui.autocomplete.css",
-                "~/Content/themes/base/jquery.ui.button.css",
-                "~/Content/themes/base/jquery.ui.dialog.css",
-                "~/Content/themes/base/jquery.ui.slider.css",
-                "~/Content/themes/base/jquery.ui.tabs.css",
-                "~/Content/themes/base/jquery.ui.datepicker.css",
-                "~/Content/themes/base/jquery.ui.progressbar.css",
-                "~/Content/themes/base/jquery.ui.theme.css"));
 
             if (IncludeGoogleAnalytics)
             {
