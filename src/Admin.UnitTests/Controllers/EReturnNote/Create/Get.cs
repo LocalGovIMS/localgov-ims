292673cbd241cdf5c7ce49e0f67b4d8d86981e1c
BMBC Dev Detec <94853739+bmbc-dev-detec@users.noreply.github.com>
2023-01-19 16:16:10 +0000

    Simplify Admin EReturnNoteController unit tests (#478)

--- src/Admin.UnitTests/Controllers/EReturnNote/Create/Get.cs
+++ src/Admin.UnitTests/Controllers/EReturnNote/Create/Get.cs
@@ -1,49 +1,29 @@
-﻿using Admin.Interfaces.Commands;
-using Admin.Interfaces.ModelBuilders;
-using log4net;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
-using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Web.Mvc;
 using Web.Mvc.Navigation;
-using Controller = Admin.Controllers.EReturnNoteController;
-using ControllerDependencies = Admin.Controllers.EReturnNoteControllerDependencies;
-using EditViewModel = Admin.Models.EReturnNote.EditViewModel;
-using ListViewModel = Admin.Models.EReturnNote.ListViewModel;
 
 namespace Admin.UnitTests.Controllers.EReturnNote.Create
 {
     [TestClass]
     [ExcludeFromCodeCoverage]
-    public class Get
+    public class Get : TestBase
     {
-        private readonly Type _controller = typeof(Controller);
-
-        private readonly Mock<ILog> _mockLogger = new Mock<ILog>();
-        private readonly Mock<IModelBuilder<ListViewModel, int>> _mockListViewModelBuilder = new Mock<IModelBuilder<ListViewModel, int>>();
-        private readonly Mock<IModelCommand<EditViewModel>> _mockCreateCommand = new Mock<IModelCommand<EditViewModel>>();
+        public Get()
+        {
+            SetupController();
+        }
 
         private MethodInfo GetMethod()
         {
-            return _controller.GetMethods()
-                .Where(x => x.CustomAttributes.Any(y => y.AttributeType == typeof(HttpGetAttribute)))
-                .Where(x => x.Name == "Create")
-                .FirstOrDefault();
+            return GetMethod(typeof(HttpGetAttribute), "Create");
         }
 
         private ActionResult GetResult()
         {
-            var dependencies = new ControllerDependencies(
-                _mockLogger.Object,
-                _mockListViewModelBuilder.Object,
-                _mockCreateCommand.Object);
-
-            var controller = new Controller(dependencies);
-
-            return controller.Create(1);
+            return Controller.Create(1);
         }
 
         [TestMethod]
@@ -107,7 +87,7 @@ namespace Admin.UnitTests.Controllers.EReturnNote.Create
             var result = GetResult() as ViewResult;
 
             Assert.IsNotNull(result);
-            Assert.IsInstanceOfType(result.Model, typeof(EditViewModel));
+            Assert.IsInstanceOfType(result.Model, typeof(Models.EReturnNote.EditViewModel));
         }
     }
 }
---
+++ src/Admin.UnitTests/Controllers/EReturnNote/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using Admin.Interfaces.Commands;
+using Admin.Interfaces.ModelBuilders;
+using log4net;
+using Moq;
+using System.Linq;
+using System.Reflection;
+using System;
+using Controller = Admin.Controllers.EReturnNoteController;
+using Dependencies = Admin.Controllers.EReturnNoteControllerDependencies;
+
+namespace Admin.UnitTests.Controllers.EReturnNote
+{
+    public class TestBase
+    {
+        protected Controller Controller;
+
+        protected readonly Mock<ILog> MockLogger = new Mock<ILog>();
+        protected readonly Mock<IModelBuilder<Admin.Models.EReturnNote.ListViewModel, int>> MockListViewModelBuilder = new Mock<IModelBuilder<Admin.Models.EReturnNote.ListViewModel, int>>();
+        protected readonly Mock<IModelCommand<Admin.Models.EReturnNote.EditViewModel>> MockCreateCommand = new Mock<IModelCommand<Admin.Models.EReturnNote.EditViewModel>>();
+
+        protected void SetupController()
+        {
+            var dependencies = new Dependencies(
+                    MockLogger.Object,
+                    MockListViewModelBuilder.Object,
+                    MockCreateCommand.Object);
+
+            Controller = new Controller(dependencies);
+        }
+
+        protected MethodInfo GetMethod(Type attributeType, string name)
+        {
+            return typeof(Controller).GetMethods()
+                .Where(x => x.CustomAttributes.Any(y => y.AttributeType == attributeType))
+                .Where(x => x.Name == name)
+                .FirstOrDefault();
+        }
+    }
+}
