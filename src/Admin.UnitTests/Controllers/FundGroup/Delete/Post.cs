f8516daa18608f5fc37b3ba7554016a8c3b90855
BMBC Detec <bmbc.dev@detec.software>
2023-01-20 09:49:43 +0000

    Simplify Admin FundGroupController unit tests

--- src/Admin.UnitTests/Controllers/FundGroup/Delete/Post.cs
+++ src/Admin.UnitTests/Controllers/FundGroup/Delete/Post.cs
@@ -1,64 +1,36 @@
-﻿using Admin.Controllers;
-using Admin.Interfaces.Commands;
-using Admin.Interfaces.ModelBuilders;
-using log4net;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Web.Mvc;
-using Controller = Admin.Controllers.FundGroupController;
 
 namespace Admin.UnitTests.Controllers.FundGroup.Delete
 {
     [TestClass]
     [ExcludeFromCodeCoverage]
-    public class Post
+    public class Post : TestBase
     {
-        private readonly Type _controller = typeof(Controller);
-
-        private readonly Mock<ILog> _mockLogger = new Mock<ILog>();
-        private readonly Mock<IModelBuilder<Models.FundGroup.DetailsViewModel, int>> _mockDetailsViewModelBuilder = new Mock<IModelBuilder<Models.FundGroup.DetailsViewModel, int>>();
-        private readonly Mock<IModelBuilder<Models.FundGroup.EditViewModel, int>> _mockCreateViewModelBuilder = new Mock<IModelBuilder<Models.FundGroup.EditViewModel, int>>();
-        private readonly Mock<IModelBuilder<Models.FundGroup.EditViewModel, int>> _mockEditViewModelBuilder = new Mock<IModelBuilder<Models.FundGroup.EditViewModel, int>>();
-        private readonly Mock<IModelBuilder<IList<Models.FundGroup.DetailsViewModel>, int>> _mockListViewModelBuilder = new Mock<IModelBuilder<IList<Models.FundGroup.DetailsViewModel>, int>>();
-        private readonly Mock<IModelCommand<Models.FundGroup.EditViewModel>> _mockCreateCommand = new Mock<IModelCommand<Models.FundGroup.EditViewModel>>();
-        private readonly Mock<IModelCommand<Models.FundGroup.EditViewModel>> _mockEditCommand = new Mock<IModelCommand<Models.FundGroup.EditViewModel>>();
+        public Post()
+        {
+            SetupController();
+        }
 
         private MethodInfo GetMethod()
         {
-            return _controller.GetMethods()
-                .Where(x => x.CustomAttributes.Any(y => y.AttributeType == typeof(HttpPostAttribute)))
-                .Where(x => x.Name == "DeleteFundGroup")
-                .FirstOrDefault();
+            return GetMethod(typeof(HttpPostAttribute), "DeleteFundGroup");
         }
 
         private ActionResult GetResult(int id, bool isModelValid)
         {
-            var deleteCommand = new Mock<IModelCommand<int>>();
-            deleteCommand.Setup(x => x.Execute(It.IsAny<int>())).Returns(new Admin.Classes.Commands.CommandResult(true));
-
-            var dependencies = new FundGroupControllerDependencies(
-                _mockLogger.Object,
-                _mockDetailsViewModelBuilder.Object,
-                _mockCreateViewModelBuilder.Object,
-                _mockEditViewModelBuilder.Object,
-                _mockListViewModelBuilder.Object,
-                _mockCreateCommand.Object,
-                _mockEditCommand.Object,
-                deleteCommand.Object);
-
-            var controller = new Controller(dependencies);
+            MockDeleteCommand.Setup(x => x.Execute(It.IsAny<int>())).Returns(new Admin.Classes.Commands.CommandResult(true));
 
             if (!isModelValid)
             {
-                controller.ModelState.AddModelError("userId", "error");
+                Controller.ModelState.AddModelError("userId", "error");
             }
 
-            return controller.DeleteFundGroup(id);
+            return Controller.DeleteFundGroup(id);
         }
 
         [TestMethod]
