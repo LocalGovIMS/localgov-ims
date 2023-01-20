0de8b0bb28db6c19bd701a23a3cea5b90e1e921d
BMBC Detec <bmbc.dev@detec.software>
2023-01-20 10:21:57 +0000

    Simplify Admin FundMessageMetadataController unit tests

--- src/Admin.UnitTests/Controllers/FundMessageMetadata/ListForFundMessage/Get.cs
+++ src/Admin.UnitTests/Controllers/FundMessageMetadata/ListForFundMessage/Get.cs
@@ -1,75 +1,43 @@
-﻿using Admin.Classes.ViewModelBuilders.FundMessageMetadata;
-using Admin.Interfaces.Commands;
-using Admin.Interfaces.ModelBuilders;
-using log4net;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Web.Mvc;
-using Controller = Admin.Controllers.FundMessageMetadataController;
-using ControllerDependencies = Admin.Controllers.FundMessageMetadataControllerDependencies;
-using DetailsViewModel = Admin.Models.FundMessageMetadata.DetailsViewModel;
-using EditViewModel = Admin.Models.FundMessageMetadata.EditViewModel;
-using ListViewModel = Admin.Models.FundMessageMetadata.ListViewModel;
-using SearchCriteria = Admin.Models.FundMessageMetadata.SearchCriteria;
 
 namespace Admin.UnitTests.Controllers.FundMessageMetadata.ListForFundMessage
 {
     [TestClass]
     [ExcludeFromCodeCoverage]
-    public class Get
+    public class Get : TestBase
     {
-        private readonly Type _controller = typeof(Controller);
-
-        private readonly Mock<ILog> _mockLogger = new Mock<ILog>();
-        private readonly Mock<IModelBuilder<DetailsViewModel, int>> _mockDetailsViewModelBuilder = new Mock<IModelBuilder<DetailsViewModel, int>>();
-        private readonly Mock<IModelBuilder<EditViewModel, CreateViewModelBuilderArgs>> _mockCreateViewModelBuilder = new Mock<IModelBuilder<EditViewModel, CreateViewModelBuilderArgs>>();
-        private readonly Mock<IModelBuilder<EditViewModel, int>> _mockEditViewModelBuilder = new Mock<IModelBuilder<EditViewModel, int>>();
-        private readonly Mock<IModelBuilder<ListViewModel, SearchCriteria>> _mockListViewModelBuilder = new Mock<IModelBuilder<ListViewModel, SearchCriteria>>();
-        private readonly Mock<IModelCommand<EditViewModel>> _mockCreateCommand = new Mock<IModelCommand<EditViewModel>>();
-        private readonly Mock<IModelCommand<EditViewModel>> _mockEditCommand = new Mock<IModelCommand<EditViewModel>>();
-        private readonly Mock<IModelCommand<int>> _mockDeleteCommand = new Mock<IModelCommand<int>>();
-
-        private MethodInfo GetListMethod()
+        public Get()
         {
-            return _controller.GetMethods()
-                .Where(x => x.Name == "_ListForFundMessage")
-                .FirstOrDefault();
+            SetupController();
+        }
+
+        private MethodInfo GetMethod()
+        {
+            return GetMethod(typeof(ChildActionOnlyAttribute), "_ListForFundMessage");
         }
 
         [TestMethod]
         public void HasCorrectNumberOfCustomAttributes()
         {
-            Assert.AreEqual(1, GetListMethod().CustomAttributes.Count());
+            Assert.AreEqual(1, GetMethod().CustomAttributes.Count());
         }
 
         [TestMethod]
         public void HasASingleChildActionOnlyAttribute()
         {
-            Assert.AreEqual(1, GetListMethod().CustomAttributes.Where(ca => ca.AttributeType == typeof(ChildActionOnlyAttribute)).Count());
+            Assert.AreEqual(1, GetMethod().CustomAttributes.Where(ca => ca.AttributeType == typeof(ChildActionOnlyAttribute)).Count());
         }
 
         private ActionResult GetTestListResult()
         {
-            var listViewModelBuilder = new Mock<IModelBuilder<ListViewModel, SearchCriteria>>();
-            listViewModelBuilder.Setup(x => x.Build(It.IsAny<SearchCriteria>())).Returns(new ListViewModel());
+            MockListViewModelBuilder.Setup(x => x.Build(It.IsAny<Models.FundMessageMetadata.SearchCriteria>())).Returns(new Models.FundMessageMetadata.ListViewModel());
 
-            var dependencies = new ControllerDependencies(
-                _mockLogger.Object,
-                _mockDetailsViewModelBuilder.Object,
-                _mockCreateViewModelBuilder.Object,
-                _mockEditViewModelBuilder.Object,
-                listViewModelBuilder.Object,
-                _mockCreateCommand.Object,
-                _mockEditCommand.Object,
-                _mockDeleteCommand.Object);
-
-            var controller = new Controller(dependencies);
-
-            return controller._ListForFundMessage(1);
+            return Controller._ListForFundMessage(1);
         }
 
         [TestMethod]
