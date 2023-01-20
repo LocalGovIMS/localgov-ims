6db68ca5b4f62458ab7c82af068d830a2350f047
BMBC Dev Detec <94853739+bmbc-dev-detec@users.noreply.github.com>
2023-01-20 09:22:33 +0000

    Simplify Admin FileImportController unit tests (#483)

---
+++ src/Admin.UnitTests/Controllers/FileImport/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using Admin.Interfaces.Commands;
+using log4net;
+using Moq;
+using System.Linq;
+using System.Reflection;
+using System;
+using Controller = Admin.Controllers.FileImportController;
+using Dependencies = Admin.Controllers.FileImportControllerDependencies;
+using Admin.Classes.Commands.FileImport;
+
+namespace Admin.UnitTests.Controllers.FileImport
+{
+    public class TestBase
+    {
+        protected Controller Controller;
+
+        protected readonly Mock<ILog> MockLogger = new Mock<ILog>();
+        protected readonly Mock<IModelCommand<SaveCommandArgs>> MockSaveCommand = new Mock<IModelCommand<SaveCommandArgs>>();
+        protected readonly Mock<IModelCommand<int>> MockProcessCommand = new Mock<IModelCommand<int>>();
+
+        protected void SetupController()
+        { 
+            var dependencies = new Dependencies(
+                    MockLogger.Object,
+                    MockSaveCommand.Object,
+                    MockProcessCommand.Object);
+
+            Controller = new Controller(dependencies);
+        }
+
+        protected MethodInfo GetMethod(Type attributeType, string name)
+        {
+            return typeof(Controller).GetMethods()
+                .Where(x => x.CustomAttributes.Any(y => y.AttributeType == attributeType))
+                .Where(x => x.Name == name)
+                .FirstOrDefault();
+        }
+    }
+}
