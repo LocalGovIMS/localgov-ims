369601a5430ce3d576dccfad3ec28fe9aee961bf
BMBC Dev Detec <94853739+bmbc-dev-detec@users.noreply.github.com>
2023-01-20 15:40:34 +0000

    Simplify Admin ImportTypeImportProcessingRuleController unit tests (#507)

--- src/Admin.UnitTests/Controllers/ImportTypeImportProcessingRule/List/Get.cs
+++ src/Admin.UnitTests/Controllers/ImportTypeImportProcessingRule/List/Get.cs
@@ -1,17 +1,9 @@
-﻿using Admin.Interfaces.Commands;
-using Admin.Interfaces.ModelBuilders;
-using log4net;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Web.Mvc;
-using Controller = Admin.Controllers.ImportTypeImportProcessingRuleController;
-using ControllerDependencies = Admin.Controllers.ImportTypeImportProcessingRuleControllerDependencies;
-using DetailsViewModel = Admin.Models.ImportTypeImportProcessingRule.DetailsViewModel;
-using EditViewModel = Admin.Models.ImportTypeImportProcessingRule.EditViewModel;
 using ListViewModel = Admin.Models.ImportTypeImportProcessingRule.ListViewModel;
 using SearchCriteria = Admin.Models.ImportTypeImportProcessingRule.SearchCriteria;
 
@@ -19,56 +11,35 @@ namespace Admin.UnitTests.Controllers.ImportTypeImportProcessingRule.List
 {
     [TestClass]
     [ExcludeFromCodeCoverage]
-    public class Get
+    public class Get : TestBase
     {
-        private readonly Type _controller = typeof(Controller);
-
-        private readonly Mock<ILog> _mockLogger = new Mock<ILog>();
-        private readonly Mock<IModelBuilder<DetailsViewModel, int>> _mockDetailsViewModelBuilder = new Mock<IModelBuilder<DetailsViewModel, int>>();
-        private readonly Mock<IModelBuilder<EditViewModel, int>> _mockCreateViewModelBuilder = new Mock<IModelBuilder<EditViewModel, int>>();
-        private readonly Mock<IModelBuilder<EditViewModel, int>> _mockEditViewModelBuilder = new Mock<IModelBuilder<EditViewModel, int>>();
-        private readonly Mock<IModelBuilder<ListViewModel, SearchCriteria>> _mockListViewModelBuilder = new Mock<IModelBuilder<ListViewModel, SearchCriteria>>();
-        private readonly Mock<IModelCommand<EditViewModel>> _mockCreateCommand = new Mock<IModelCommand<EditViewModel>>();
-        private readonly Mock<IModelCommand<EditViewModel>> _mockEditCommand = new Mock<IModelCommand<EditViewModel>>();
-        private readonly Mock<IModelCommand<int>> _mockDeleteCommand = new Mock<IModelCommand<int>>();
-
-        private MethodInfo GetListMethod()
+        public Get()
         {
-            return _controller.GetMethods()
-                .Where(x => x.Name == "_List")
-                .FirstOrDefault();
+            SetupController();
+        }
+
+        private MethodInfo GetMethod()
+        {
+            return GetMethod(typeof(ChildActionOnlyAttribute), nameof(Controller._List));
         }
 
         [TestMethod]
         public void HasCorrectNumberOfCustomAttributes()
         {
-            Assert.AreEqual(1, GetListMethod().CustomAttributes.Count());
+            Assert.AreEqual(1, GetMethod().CustomAttributes.Count());
         }
 
         [TestMethod]
         public void HasASingleChildActionOnlyAttribute()
         {
-            Assert.AreEqual(1, GetListMethod().CustomAttributes.Where(ca => ca.AttributeType == typeof(ChildActionOnlyAttribute)).Count());
+            Assert.AreEqual(1, GetMethod().CustomAttributes.Where(ca => ca.AttributeType == typeof(ChildActionOnlyAttribute)).Count());
         }
 
         private ActionResult GetTestListResult()
         {
-            var listViewModelBuilder = new Mock<IModelBuilder<ListViewModel, SearchCriteria>>();
-            listViewModelBuilder.Setup(x => x.Build(It.IsAny<SearchCriteria>())).Returns(new ListViewModel());
+            MockListViewModelBuilder.Setup(x => x.Build(It.IsAny<SearchCriteria>())).Returns(new ListViewModel());
 
-            var dependencies = new ControllerDependencies(
-                _mockLogger.Object,
-                _mockDetailsViewModelBuilder.Object,
-                _mockCreateViewModelBuilder.Object,
-                _mockEditViewModelBuilder.Object,
-                listViewModelBuilder.Object,
-                _mockCreateCommand.Object,
-                _mockEditCommand.Object,
-                _mockDeleteCommand.Object);
-
-            var controller = new Controller(dependencies);
-
-            return controller._List(1);
+            return Controller._List(1);
         }
 
         [TestMethod]
