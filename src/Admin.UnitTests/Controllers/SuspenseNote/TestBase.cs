c0c9049fc1d5f397a0fe1ded598a61beb66acec9
BMBC Detec <bmbc.dev@detec.software>
2023-01-23 10:03:49 +0000

    Simplify Admin SuspenseNoteController unit tests

---
+++ src/Admin.UnitTests/Controllers/SuspenseNote/TestBase.cs
@@ -0,0 +1,40 @@
+﻿using Admin.Interfaces.Commands;
+using Admin.Interfaces.ModelBuilders;
+using Admin.Models.SuspenseNote;
+using log4net;
+using Moq;
+using System;
+using System.Linq;
+using System.Reflection;
+using Controller = Admin.Controllers.SuspenseNoteController;
+using Dependencies = Admin.Controllers.SuspenseNoteControllerDependencies;
+
+namespace Admin.UnitTests.Controllers.SuspenseNote
+{
+    public class TestBase
+    {
+        protected Controller Controller;
+
+        protected readonly Mock<ILog> MockLogger = new Mock<ILog>();
+        protected readonly Mock<IModelBuilder<ListViewModel, int>> MockListViewModelBuilder = new Mock<IModelBuilder<ListViewModel, int>>();
+        protected readonly Mock<IModelCommand<EditViewModel>> MockCreateCommand = new Mock<IModelCommand<EditViewModel>>();
+
+        protected void SetupController()
+        {
+            var dependencies = new Dependencies(
+                    MockLogger.Object,
+                    MockListViewModelBuilder.Object,
+                    MockCreateCommand.Object);
+
+            Controller = new Controller(dependencies);
+        }
+
+        protected MethodInfo GetMethod(Type attributeType, string name)
+        {
+            return typeof(Controller).GetMethods()
+                .Where(x => x.CustomAttributes.Any(y => y.AttributeType == attributeType))
+                .Where(x => x.Name == name)
+                .FirstOrDefault();
+        }
+    }
+}
