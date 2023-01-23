4e41d149807e7d44c4a4bb111c164d6a49cf5143
BMBC Detec <bmbc.dev@detec.software>
2023-01-23 10:42:55 +0000

    Simplify Admin TransferController unit tests

--- src/Admin.UnitTests/Controllers/Transfer/SubmitTransfers/Post.cs
+++ src/Admin.UnitTests/Controllers/Transfer/SubmitTransfers/Post.cs
@@ -1,55 +1,34 @@
-﻿using Admin.Interfaces.Commands;
-using Admin.Interfaces.ModelBuilders;
-using Admin.Models.Transfer;
-using BusinessLogic.Models;
-using log4net;
+﻿using Admin.Models.Transfer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Web.Mvc;
-using Controller = Admin.Controllers.TransferController;
-using Dependencies = Admin.Controllers.TransferControllerDependencies;
 
 namespace Admin.UnitTests.Controllers.Transfer.SubmitTransfers
 {
     [TestClass]
     [ExcludeFromCodeCoverage]
-    public class Post
+    public class Post : TestBase
     {
-        private readonly Type _controller = typeof(Controller);
-
-        private readonly Mock<ILog> _mockLogger = new Mock<ILog>();
-        private readonly Mock<IModelBuilder<TransferViewModel, string>> _mockTransferViewModelBuilder = new Mock<IModelBuilder<TransferViewModel, string>>();
-        private readonly Mock<IModelCommand<TransferItem>> _mockValidateTransferItemCommand = new Mock<IModelCommand<TransferItem>>();
+        public Post()
+        {
+            SetupController();
+        }
 
         private MethodInfo GetMethod()
         {
-            return _controller.GetMethods()
-                .Where(x => x.CustomAttributes.Any(y => y.AttributeType == typeof(HttpPostAttribute)))
-                .Where(x => x.Name == "SubmitTransfers")
-                .FirstOrDefault();
+            return GetMethod(typeof(HttpPostAttribute), nameof(Controller.SubmitTransfers));
         }
 
-        private ActionResult GetResult(TransferViewModel model, bool throwException)
+        private ActionResult GetResult(TransferViewModel model)
         {
-            var transferCommand = new Mock<IModelCommand<TransferViewModel>>();
-
-            transferCommand.Setup(x => x.Execute(
+            MockTransferCommand.Setup(x => x.Execute(
                 It.IsAny<TransferViewModel>()))
                 .Returns(new Admin.Classes.Commands.CommandResult(true) { Data = "TestMessage" });
 
-            var dependencies = new Dependencies(
-                _mockLogger.Object,
-                _mockTransferViewModelBuilder.Object,
-                transferCommand.Object,
-                _mockValidateTransferItemCommand.Object);
-
-            var controller = new Controller(dependencies);
-
-            return controller.SubmitTransfers(model);
+           return Controller.SubmitTransfers(model);
         }
 
         [TestMethod]
@@ -67,7 +46,7 @@ namespace Admin.UnitTests.Controllers.Transfer.SubmitTransfers
         [TestMethod]
         public void ReturnsJsonResult()
         {
-            var result = GetResult(null, false);
+            var result = GetResult(null);
 
             Assert.IsInstanceOfType(result, typeof(JsonResult));
             Assert.AreEqual(((JsonResult)result).Data.ToString(), "{ ok = True, message = TestMessage }");
