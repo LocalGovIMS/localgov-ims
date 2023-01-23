1150a8b08e47c886f93bd1ffef069c98423d2cd2
BMBC Dev Detec <94853739+bmbc-dev-detec@users.noreply.github.com>
2023-01-23 09:23:51 +0000

    Simplify Admin OfficeController unit tests (#516)

--- src/Admin.UnitTests/Controllers/Office/Edit/Get.cs
+++ src/Admin.UnitTests/Controllers/Office/Edit/Get.cs
@@ -1,56 +1,32 @@
-﻿using Admin.Interfaces.Commands;
-using Admin.Interfaces.ModelBuilders;
-using log4net;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Web.Mvc;
 using Web.Mvc.Navigation;
-using Controller = Admin.Controllers.OfficeController;
-using Dependencies = Admin.Controllers.OfficeControllerDependencies;
 
 namespace Admin.UnitTests.Controllers.Office.Edit
 {
     [TestClass]
     [ExcludeFromCodeCoverage]
-    public class Get
+    public class Get : TestBase
     {
-        private readonly Type _controller = typeof(Controller);
-
-        private readonly Mock<ILog> _mockLogger = new Mock<ILog>();
-        private readonly Mock<IModelBuilder<Models.Office.DetailsViewModel, string>> _mockDetailsViewModelBuilder = new Mock<IModelBuilder<Models.Office.DetailsViewModel, string>>();
-        private readonly Mock<IModelBuilder<IList<Models.Office.DetailsViewModel>, string>> _mockListViewModelBuilder = new Mock<IModelBuilder<IList<Models.Office.DetailsViewModel>, string>>();
-        private readonly Mock<IModelCommand<Models.Office.EditViewModel>> _mockCreateCommand = new Mock<IModelCommand<Models.Office.EditViewModel>>();
-        private readonly Mock<IModelCommand<Models.Office.EditViewModel>> _mockEditCommand = new Mock<IModelCommand<Models.Office.EditViewModel>>();
+        public Get()
+        {
+            SetupController();
+        }
 
         private MethodInfo GetMethod()
         {
-            return _controller.GetMethods()
-                .Where(x => x.CustomAttributes.Any(y => y.AttributeType == typeof(HttpGetAttribute)))
-                .Where(x => x.Name == "Edit")
-                .FirstOrDefault();
+            return GetMethod(typeof(HttpGetAttribute), nameof(Controller.Edit));
         }
 
         private ActionResult GetResult()
         {
-            var editViewModelBuilder = new Mock<IModelBuilder<Models.Office.EditViewModel, string>>();
-            editViewModelBuilder.Setup(x => x.Build(It.IsAny<string>())).Returns(new Models.Office.EditViewModel());
+            MockEditViewModelBuilder.Setup(x => x.Build(It.IsAny<string>())).Returns(new Models.Office.EditViewModel());
 
-            var dependencies = new Dependencies(
-                _mockLogger.Object,
-                _mockDetailsViewModelBuilder.Object,
-                editViewModelBuilder.Object,
-                _mockListViewModelBuilder.Object,
-                _mockCreateCommand.Object,
-                _mockEditCommand.Object);
-
-            var controller = new Controller(dependencies);
-
-            return controller.Edit("F1");
+            return Controller.Edit("F1");
         }
 
         [TestMethod]
--- src/Admin.UnitTests/Controllers/Office/Edit/Post.cs
+++ src/Admin.UnitTests/Controllers/Office/Edit/Post.cs
@@ -1,60 +1,36 @@
-﻿using Admin.Interfaces.Commands;
-using Admin.Interfaces.ModelBuilders;
-using log4net;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Web.Mvc;
-using Controller = Admin.Controllers.OfficeController;
-using Dependencies = Admin.Controllers.OfficeControllerDependencies;
 
 namespace Admin.UnitTests.Controllers.Office.Edit
 {
     [TestClass]
     [ExcludeFromCodeCoverage]
-    public class Post
+    public class Post : TestBase
     {
-        private readonly Type _controller = typeof(Controller);
-
-        private readonly Mock<ILog> _mockLogger = new Mock<ILog>();
-        private readonly Mock<IModelBuilder<Models.Office.DetailsViewModel, string>> _mockDetailsViewModelBuilder = new Mock<IModelBuilder<Models.Office.DetailsViewModel, string>>();
-        private readonly Mock<IModelBuilder<Models.Office.EditViewModel, string>> _mockEditViewModelBuilder = new Mock<IModelBuilder<Models.Office.EditViewModel, string>>();
-        private readonly Mock<IModelBuilder<IList<Models.Office.DetailsViewModel>, string>> _mockListViewModelBuilder = new Mock<IModelBuilder<IList<Models.Office.DetailsViewModel>, string>>();
-        private readonly Mock<IModelCommand<Models.Office.EditViewModel>> _mockCreateCommand = new Mock<IModelCommand<Models.Office.EditViewModel>>();
+        public Post()
+        {
+            SetupController();
+        }
 
         private MethodInfo GetMethod()
         {
-            return _controller.GetMethods()
-                .Where(x => x.CustomAttributes.Any(y => y.AttributeType == typeof(HttpPostAttribute)))
-                .Where(x => x.Name == "Edit")
-                .FirstOrDefault();
+            return GetMethod(typeof(HttpPostAttribute), nameof(Controller.Create));
         }
 
         private ActionResult GetResult(Models.Office.EditViewModel model, bool isModelValid)
         {
-            var editCommand = new Mock<IModelCommand<Models.Office.EditViewModel>>();
-            editCommand.Setup(x => x.Execute(It.IsAny<Models.Office.EditViewModel>())).Returns(new Admin.Classes.Commands.CommandResult(true));
-
-            var dependencies = new Dependencies(
-                _mockLogger.Object,
-                _mockDetailsViewModelBuilder.Object,
-                _mockEditViewModelBuilder.Object,
-                _mockListViewModelBuilder.Object,
-                _mockCreateCommand.Object,
-                editCommand.Object);
-
-            var controller = new Controller(dependencies);
+            MockEditCommand.Setup(x => x.Execute(It.IsAny<Models.Office.EditViewModel>())).Returns(new Admin.Classes.Commands.CommandResult(true));
 
             if (!isModelValid)
             {
-                controller.ModelState.AddModelError("userId", "error");
+                Controller.ModelState.AddModelError("userId", "error");
             }
 
-            return controller.Edit(model);
+            return Controller.Edit(model);
         }
 
         [TestMethod]
