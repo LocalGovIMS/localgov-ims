46c84c8ff76e2bc3c98c2607f240d63b8eb18ff7
BMBC Dev Detec <94853739+bmbc-dev-detec@users.noreply.github.com>
2023-01-27 11:09:39 +0000

    Increase test coverage of BusinessLogic TransactionFeeValidator (#565)

---
+++ src/BusinessLogic.UnitTests/Validators/TransactionFee/BaseTransactionFeeTest.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLogic.Interfaces.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BusinessLogic.UnitTests.Validators.TransactionFee
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class BaseTransactionFeeTest
+    {
+        protected readonly Mock<ITransactionService> MockTransactionService = new Mock<ITransactionService>();
+
+        public BusinessLogic.Validators.TransactionFeeValidator GetValidator()
+        {
+            var validator = new BusinessLogic.Validators.TransactionFeeValidator(MockTransactionService.Object);
+
+            return validator;
+        }
+    }
+}
