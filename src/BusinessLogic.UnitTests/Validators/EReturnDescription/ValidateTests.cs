f77df5f982c7320f35d44a9ac1c1729c18ba51f6
BMBC Dev Detec <94853739+bmbc-dev-detec@users.noreply.github.com>
2023-01-27 09:38:00 +0000

    Increase test coverage of BusinessLogic EReturnDescriptionValidator (#561)

---
+++ src/BusinessLogic.UnitTests/Validators/EReturnDescription/ValidateTests.cs
@@ -0,0 +1,95 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BusinessLogic.UnitTests.Validators.EReturnDescription
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class ValidateTests : BaseEReturnDescriptionTest
+    {
+        private const string DefaultReference = "ABC123";
+        private const string DefaultDescription = "DefaultDescription";
+
+        private void SetupService(bool descriptionOverride)
+        {
+            MockEReturnTemplateRowService.Setup(x => x.Get(It.IsAny<int>()))
+                .Returns(new Entities.TemplateRow
+                    {
+                        Description = DefaultDescription,
+                        DescriptionOverride = descriptionOverride,
+                        Reference = DefaultReference
+                }
+                );
+        }
+
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow(" ")]
+        [TestMethod]
+        public void WhenDescriptionIsNullOrEmptyThenTheExpectedResultIsReturned(string description)
+        {
+            // Arrange
+            SetupService(false);
+            var validator = GetEReturnDescriptionValidator();
+            
+            // Act
+            var result = validator.Validate(description, 1);
+
+            // Assert
+            result.Success.Should().BeFalse();
+        }
+
+        [DataRow("An non matching description", false)]
+        [DataRow(DefaultDescription, true)]
+        [TestMethod]
+        public void WhenDescriptionCannotBeOverwrittenThenThenExpectedResultIsReturned(string description, bool success)
+        {
+            // Arrange
+            SetupService(false);
+            var validator = GetEReturnDescriptionValidator();
+
+            // Act
+            var result = validator.Validate(description, 1);
+
+            // Assert
+            result.Success.Should().Be(success);
+        }
+
+        [DataRow("An non matching description")]
+        [DataRow("Another description")]
+        [TestMethod]
+        public void WhenDescriptionCannotBeOverwrittenAndTheDescriptionDoesNotMatchThenTheExpectedErrorMessageIsReturned(string description)
+        {
+            // Arrange
+            SetupService(false);
+            var validator = GetEReturnDescriptionValidator();
+
+            // Act
+            var result = validator.Validate(description, 1);
+
+            // Assert
+            result.Error.Should().Be($"Description '{DefaultReference}' cannot be overridden, but is set to '{description}'");
+        }
+
+        [DataRow("A description")]
+        [DataRow("Another description")]
+        [DataRow("Yet another description")]
+        [TestMethod]
+        public void WhenDescriptionCanBeOverriddenThenTheExpectedResultIsReturned(string description)
+        {
+            // Arrange
+            SetupService(true);
+            var validator = GetEReturnDescriptionValidator();
+
+            // Act
+            var result = validator.Validate(description, 1);
+
+            // Assert
+            result.Success.Should().BeTrue();
+        }
+    }
+}
+
