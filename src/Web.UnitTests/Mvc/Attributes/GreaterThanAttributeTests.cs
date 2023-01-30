ce15f8a7bdc8b71e7d5efb233916c37541e0a64c
BMBC Detec <bmbc.dev@detec.software>
2023-01-30 09:35:30 +0000

    Increase test coverage of Web GreaterThanAttribute

--- src/Web.UnitTests/Mvc/Attributes/GreaterThanAttributeTests.cs
+++ src/Web.UnitTests/Mvc/Attributes/GreaterThanAttributeTests.cs
@@ -12,6 +12,22 @@ namespace Web.UnitTests.Mvc.Attributes
     public class GreaterThanAttributeTests
     {
         [TestMethod]
+        public void Constructor_throws_an_exception_when_other_property_is_null()
+        {
+            // Arrange
+            var target = new NullOtherProperty() { Min = 10, Max = 20 };
+
+            var context = new ValidationContext(target);
+            var results = new List<ValidationResult>();
+
+            // Act
+            var isValid = Validator.TryValidateObject(target, context, results, true);
+
+            // Assert
+            isValid.Should().Be(false);
+        }
+
+        [TestMethod]
         [DataRow(9, false)]
         [DataRow(10, false)]
         [DataRow(11, true)]
@@ -38,6 +54,28 @@ namespace Web.UnitTests.Mvc.Attributes
         {
             // Arrange
             var target = new GreaterThanOrEqualToTarget() { Min = 10, Max = max };
+
+            var context = new ValidationContext(target);
+            var results = new List<ValidationResult>();
+
+            // Act
+            var isValid = Validator.TryValidateObject(target, context, results, true);
+
+            // Assert
+            isValid.Should().Be(expectedResult);
+        }
+
+        [TestMethod]
+        [DataRow("9", false)]
+        [DataRow("10", true)]
+        [DataRow("10.01", true)]
+        [DataRow("10X", false)]
+        [DataRow("Ten", false)]
+        [DataRow("%$£", false)]
+        public void IsValid_returns_the_expected_result_when_the_value_provided_is_not_a_decimal(string value, bool expectedResult)
+        {
+            // Arrange
+            var target = new InvalidDataTypeTarget() { Min = 10, Max = value };
 
             var context = new ValidationContext(target);
             var results = new List<ValidationResult>();
@@ -79,6 +117,42 @@ namespace Web.UnitTests.Mvc.Attributes
             Assert.AreEqual(attribute.ValidOn, AttributeTargets.Method);
         }
 
+        [DataRow("AProperty", "*.AProperty")]
+        [DataRow("AnotherProperty", "*.AnotherProperty")]
+        [TestMethod]
+        public void FormatPropertyForClientValidation_returns_a_correctly_formatted_value(string property, string expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var result = GreaterThanAttribute.FormatPropertyForClientValidation(property);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        [TestMethod]
+        public void FormatPropertyForClientValidation_throws_an_ArgumentException_if_the_value_provided_is_null()
+        {
+            // Arrange
+
+            // Act
+            Action act = () => GreaterThanAttribute.FormatPropertyForClientValidation(null);
+
+            // Assert
+            act.Should()
+                .Throw<ArgumentException>()
+                .WithMessage("Value cannot be null or empty.\r\nParameter name: property");
+        }
+
+        private class NullOtherProperty
+        {
+            public int Min { get; set; }
+
+            [GreaterThan(null, AllowEquality = true)]
+            public int Max { get; set; }
+        }
+
         private class GreaterThanTarget
         {
             public int Min { get; set; }
@@ -94,7 +168,13 @@ namespace Web.UnitTests.Mvc.Attributes
             [GreaterThan(nameof(Min), AllowEquality = true)]
             public int Max { get; set; }
         }
-    }
 
-    
+        private class InvalidDataTypeTarget
+        {
+            public int Min { get; set; }
+
+            [GreaterThan(nameof(Min), AllowEquality = true)]
+            public string Max { get; set; }
+        }
+    }
 }
