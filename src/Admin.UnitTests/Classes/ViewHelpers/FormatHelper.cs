b90f1521c0375050d2c826b94913a931c1e82247
BMBC Dev Detec <94853739+bmbc-dev-detec@users.noreply.github.com>
2023-01-30 13:41:39 +0000

    Increase test coverage of Admin FormatHelpers (#574)

--- src/Admin.UnitTests/Classes/ViewHelpers/FormatHelper.cs
+++ src/Admin.UnitTests/Classes/ViewHelpers/FormatHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Admin.Classes.ViewHelpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Web.Mvc;
 
@@ -9,21 +10,29 @@ namespace Admin.UnitTests.Classes.ViewHelpers
     [ExcludeFromCodeCoverage]
     public class FormatHelperTests
     {
+        [DataRow("10", "10.00")]
+        [DataRow("10.0", "10.00")]
+        [DataRow("10.01", "10.01")]
+        [DataRow("10.011", "10.01")]
+        [DataRow("10.001", "10.00")]
+        [DataRow("10.1", "10.10")]
+        [DataRow("10.10", "10.10")]
+        [DataRow("10.101", "10.10")]
         [TestMethod]
-        public void CanBuild()
+        public void ToCurrency_returns_the_expected_value(string value, string expectedResult)
         {
             // Arrange
             HtmlHelper helper = null;
 
             // Act
-            var result = helper.ToCurrency(10.001M);
+            var result = helper.ToCurrency(Convert.ToDecimal(value));
 
             // Assert
-            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedResult, result.ToString());
         }
 
         [TestMethod]
-        public void NullValueReturnsZero()
+        public void ToCurrency_for_a_null_value_returns_zero()
         {
             // Arrange
             HtmlHelper helper = null;
@@ -32,7 +41,49 @@ namespace Admin.UnitTests.Classes.ViewHelpers
             var result = helper.ToCurrency(null);
 
             // Assert
-            Assert.IsNotNull(result);
+            Assert.AreEqual("0.00", result.ToString());
+        }
+
+        [TestMethod]
+        public void ToMaxLength_for_a_null_value_returns_and_empty_string()
+        {
+            // Arrange
+            HtmlHelper helper = null;
+
+            // Act
+            var result = helper.ToMaxLength(null, 10);
+
+            // Assert
+            Assert.AreEqual(string.Empty, result.ToString());
+        }
+
+        [TestMethod]
+        public void ToMaxLength_for_an_empty_string_value_returns_and_empty_string()
+        {
+            // Arrange
+            HtmlHelper helper = null;
+
+            // Act
+            var result = helper.ToMaxLength(string.Empty, 10);
+
+            // Assert
+            Assert.AreEqual(string.Empty, result.ToString());
+        }
+
+        [DataRow("This is the input string", 10, "This is th...")]
+        [DataRow("This is the input string", 12, "This is the...")]
+        [DataRow("          ", 5, "...")]
+        [TestMethod]
+        public void ToMaxLength_returns_the_expected_value(string value, int length, string expectedResult)
+        {
+            // Arrange
+            HtmlHelper helper = null;
+
+            // Act
+            var result = helper.ToMaxLength(value, length);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result.ToString());
         }
     }
 }
