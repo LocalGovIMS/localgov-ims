55b128500a03edc996c0fccdef3d5da50659769f
BMBC Dev Detec <94853739+bmbc-dev-detec@users.noreply.github.com>
2023-02-02 15:52:51 +0000

    Increase test coverage of Admin UserMethodOfPaymentControllerDependencies (#582)

---
+++ src/Admin.UnitTests/Controllers/Dependencies/UserMethodOfPaymentControllerDependenciesTests.cs
@@ -0,0 +1,144 @@
+﻿using Admin.Interfaces.Commands;
+using Admin.Interfaces.ModelBuilders;
+using Admin.Models.Shared;
+using Admin.Models.UserMethodOfPayment;
+using FluentAssertions;
+using log4net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using DependenciesClass = Admin.Controllers.UserMethodOfPaymentControllerDependencies;
+
+namespace Admin.UnitTests.Classes.Dependencies
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class UserMethodOfPaymentControllerDependenciesTests
+    {
+
+        private readonly Mock<ILog> _mockLogger = new Mock<ILog>();
+        private readonly Mock<IModelBuilder<BasicListViewModel, int>> _mockBasicListViewModelBuilder = new Mock<IModelBuilder<BasicListViewModel, int>>();
+        private readonly Mock<IModelBuilder<EditViewModel, int>> _mockEditViewModelBuilder = new Mock<IModelBuilder<EditViewModel, int>>();
+        private readonly Mock<IModelCommand<EditViewModel>> _mockEditCommand = new Mock<IModelCommand<EditViewModel>>();
+
+        [TestMethod]
+        public void ThrowsCorrectExceptionTypeWhenBasicListViewModelBuiderIsNull()
+        {
+            try
+            {
+                var dependencies = new DependenciesClass(
+                    _mockLogger.Object,
+                    null,
+                    _mockEditViewModelBuilder.Object,
+                    _mockEditCommand.Object);
+
+            }
+            catch (Exception e)
+            {
+                e.Should().BeOfType(typeof(ArgumentNullException));
+            }
+        }
+
+        [TestMethod]
+        public void ThrowsCorrectExceptionDescriptionTypeWhenBasicListViewModelBuiderIsNull()
+        {
+            try
+            {
+                var dependencies = new DependenciesClass(
+                    _mockLogger.Object,
+                    null,
+                    _mockEditViewModelBuilder.Object,
+                    _mockEditCommand.Object);
+            }
+            catch (ArgumentNullException exception)
+            {
+                exception.Message.Should().Be("Value cannot be null.\r\nParameter name: basicListViewModelBuilder");
+            }
+        }
+
+        [TestMethod]
+        public void ThrowsCorrectExceptionTypeWhenEditViewModelBuiderIsNull()
+        {
+            try
+            {
+                var dependencies = new DependenciesClass(
+                    _mockLogger.Object,
+                    _mockBasicListViewModelBuilder.Object,
+                    null,
+                    _mockEditCommand.Object);
+
+            }
+            catch (Exception e)
+            {
+                e.Should().BeOfType(typeof(ArgumentNullException));
+            }
+        }
+
+        [TestMethod]
+        public void ThrowsCorrectExceptionDescriptionTypeWhenEditViewModelBuiderIsNull()
+        {
+            try
+            {
+                var dependencies = new DependenciesClass(
+                    _mockLogger.Object,
+                    _mockBasicListViewModelBuilder.Object,
+                    null,
+                    _mockEditCommand.Object);
+            }
+            catch (ArgumentNullException exception)
+            {
+                exception.Message.Should().Be("Value cannot be null.\r\nParameter name: editViewModelBuilder");
+            }
+        }
+
+        [TestMethod]
+        public void ThrowsCorrectExceptionTypeWhenEditCommandIsNull()
+        {
+            try
+            {
+                var dependencies = new DependenciesClass(
+                    _mockLogger.Object,
+                    _mockBasicListViewModelBuilder.Object,
+                    _mockEditViewModelBuilder.Object,
+                    null);
+
+            }
+            catch (Exception e)
+            {
+                e.Should().BeOfType(typeof(ArgumentNullException));
+            }
+        }
+
+        [TestMethod]
+        public void ThrowsCorrectExceptionDescriptionTypeWhenEditCommandIsNull()
+        {
+            try
+            {
+                var dependencies = new DependenciesClass(
+                    _mockLogger.Object,
+                    _mockBasicListViewModelBuilder.Object,
+                    _mockEditViewModelBuilder.Object,
+                    null);
+            }
+            catch (ArgumentNullException exception)
+            {
+                exception.Message.Should().Be("Value cannot be null.\r\nParameter name: editCommand");
+            }
+        }
+
+        [TestMethod]
+        public void CanSetProperties()
+        {
+            var dependencies = new DependenciesClass(
+                    _mockLogger.Object,
+                    _mockBasicListViewModelBuilder.Object,
+                    _mockEditViewModelBuilder.Object,
+                    _mockEditCommand.Object);
+
+            Assert.IsNotNull(dependencies.BasicListViewModelBuilder);
+            Assert.IsNotNull(dependencies.EditViewModelBuilder);
+            Assert.IsNotNull(dependencies.EditCommand);
+        }
+    }
+}
