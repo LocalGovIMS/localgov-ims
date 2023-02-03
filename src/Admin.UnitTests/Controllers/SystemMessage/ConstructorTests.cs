df809ad3cd8987f641a1dd6064c02b5b1d5d82f2
BMBC Dev Detec <94853739+bmbc-dev-detec@users.noreply.github.com>
2023-02-03 12:32:26 +0000

    Increase test coverage of Admin SystemMessageController (#590)

---
+++ src/Admin.UnitTests/Controllers/SystemMessage/ConstructorTests.cs
@@ -0,0 +1,41 @@
+﻿using Admin.Controllers;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Admin.UnitTests.Controllers.SystemMessage
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class ConstructorTests
+    {
+        [TestMethod]
+        public void ThrowsCorrectExceptionTypeIfDependenciesIsNull()
+        {
+            try
+            {
+                var controller = new SystemMessageController(null);
+
+            }
+            catch (Exception exception)
+            {
+                exception.Should().BeOfType(typeof(ArgumentNullException));
+            }
+        }
+
+        [TestMethod]
+        public void ThrowsCorrectExceptionParamNameIfDependenciesIsNull()
+        {
+            try
+            {
+                var controller = new SystemMessageController(null);
+
+            }
+            catch (ArgumentNullException exception)
+            {
+                exception.ParamName.ToLower().Should().Be("dependencies");
+            }
+        }
+    }
+}
---
+++ src/Admin.UnitTests/Controllers/SystemMessage/List/Get.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Web.Mvc;
+using Web.Mvc.Navigation;
+
+namespace Admin.UnitTests.Controllers.SystemMessage.List
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class Get : TestBase
+    {
+        public Get()
+        {
+            SetupController();
+        }
+
+        private MethodInfo GetMethod()
+        {
+            return GetMethod(typeof(HttpGetAttribute), nameof(Controller.List));
+        }
+
+        private ActionResult GetResult()
+        {
+            MockListViewModelBuilder.Setup(x => x.Build()).Returns(new List<Models.SystemMessage.DetailsViewModel>());
+
+            return Controller.List();
+        }
+
+        [TestMethod]
+        public void HasCorrectNumberOfCustomAttributes()
+        {
+            Assert.AreEqual(2, GetMethod().CustomAttributes.Count());
+        }
+
+        [TestMethod]
+        public void HasASingleNavigatablePageActionFilterAttribute()
+        {
+            Assert.AreEqual(1, GetMethod().CustomAttributes.Where(ca => ca.AttributeType == typeof(NavigatablePageActionFilterAttribute)).Count());
+        }
+
+        [TestMethod]
+        public void NavigatablePageActionFilterAttributeHasCorrectDisplayText()
+        {
+            var attribute = GetMethod().CustomAttributes.Single(ca => ca.AttributeType == typeof(NavigatablePageActionFilterAttribute));
+
+            var namedArgument = attribute.NamedArguments.Where(x => x.MemberName == "DisplayText").First();
+
+            Assert.AreEqual("System Messages", namedArgument.TypedValue.Value);
+        }
+
+        [TestMethod]
+        public void HasASingleHttpGetAttribute()
+        {
+            Assert.AreEqual(1, GetMethod().CustomAttributes.Where(ca => ca.AttributeType == typeof(HttpGetAttribute)).Count());
+        }
+
+        [TestMethod]
+        public void ReturnsAView()
+        {
+            var result = GetResult();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void ReturnsCorrectViewName()
+        {
+            var result = GetResult() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(string.IsNullOrEmpty(result.ViewName) || result.ViewName == "List");
+        }
+
+        [TestMethod]
+        public void ReturnsAViewModel()
+        {
+            var result = GetResult() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Model);
+        }
+
+        [TestMethod]
+        public void ReturnsCorrectViewModelType()
+        {
+            var result = GetResult() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(List<Models.SystemMessage.DetailsViewModel>));
+        }
+    }
+}
