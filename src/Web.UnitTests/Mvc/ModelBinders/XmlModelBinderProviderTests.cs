1945b2c2cd866da92c17d70b7709e9b1092abc0e
BMBC Detec <bmbc.dev@detec.software>
2023-02-10 11:33:24 +0000

    Add unit tests for Web.Mvc.ModelBinderProvider

---
+++ src/Web.UnitTests/Mvc/ModelBinders/XmlModelBinderProviderTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web;
+using Web.Mvc;
+using Web.UnitTests.Helpers;
+
+namespace Web.UnitTests.Mvc.ModelBinders
+{
+    [TestClass]
+    public class XmlModelBinderProviderTests
+    {
+
+        [TestMethod]
+        public void Returns_XmlModelBinder_When_ContentType_Is_TextXml()
+        {
+            // Arrange
+            HttpContext.Current = MoqHelper.FakeHttpContext("text/xml");
+            var xmlModelBinderProvider = new XmlModelBinderProvider();
+
+            // Act
+            var result = xmlModelBinderProvider.GetBinder(typeof(MyTestClass));
+
+            // Assert
+            Assert.IsInstanceOfType<XmlModelBinder>(result);
+        }
+
+        [TestMethod]
+        public void Returns_Null_When_ContentType_Is_Not_TextXml()
+        {
+            // Arrange
+            HttpContext.Current = MoqHelper.FakeHttpContext("application/json");
+            var xmlModelBinderProvider = new XmlModelBinderProvider();
+
+            // Act
+            var result = xmlModelBinderProvider.GetBinder(typeof(MyTestClass));
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+
+    public class MyTestClass
+    {
+        public MyTestClass()
+        {
+        }
+    }
+}
