7f24b5a84b8b1636d6f466c97e759da429f3324e
BMBC Dev Detec <94853739+bmbc-dev-detec@users.noreply.github.com>
2023-02-10 14:21:07 +0000

    Add unit tests for NavigatablePageActionFilterAttribute (#613)

--- src/Web.UnitTests/Helpers/MoqHelper.cs
+++ src/Web.UnitTests/Helpers/MoqHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using Moq;
+using System.IO;
 using System.Web;
 using System.Web.SessionState;
 
@@ -14,8 +15,26 @@ namespace Web.UnitTests.Helpers
         public static HttpContext FakeHttpContext(string contentType)
         {
 
-            var httpRequest = new HttpRequest("", "http://www.test.com", "");
-            httpRequest.ContentType = contentType;  
+            var httpRequest = new HttpRequest("", "http://www.test.com/test", "");
+            httpRequest.ContentType = contentType;
+
+            var stringWriter = new StringWriter();
+            var httpResponse = new HttpResponse(stringWriter);
+            var httpContext = new HttpContext(httpRequest, httpResponse);
+
+            var sessionContainer = new HttpSessionStateContainer("id", new SessionStateItemCollection(),
+                                                    new HttpStaticObjectsCollection(), 10, true,
+                                                    HttpCookieMode.AutoDetect,
+                                                    SessionStateMode.InProc, false);
+
+            SessionStateUtility.AddHttpSessionStateToContext(httpContext, sessionContainer);
+
+            return httpContext;
+        }
+
+        public static HttpContext FakeHttpContext(HttpRequest httpRequest)
+        {
+            httpRequest.ContentType = "text/xml";
 
             var stringWriter = new StringWriter();
             var httpResponse = new HttpResponse(stringWriter);
